50a48155baf966349480ed434464a953f3236aa5
Gregor Doroschenko <gregor.doroschenko@develappers.de>
2019-10-12 15:56:13 +0200

    add ng-add v1 solution

---
+++ src/ng-add/index.ts
@@ -0,0 +1,40 @@
+import { chain, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { addPackageJsonDependency, NodeDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+
+function addPackageJsonDependencies(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const dependencies: NodeDependency[] = [
+      {
+        type: NodeDependencyType.Default,
+        version: '0.0.1',
+        name: 'wdc2019'
+      }
+    ];
+
+    dependencies.forEach(dependency => {
+      addPackageJsonDependency(tree, dependency);
+      context.logger.info(`✅️ "Added ${dependency.name}" into ${dependency.type}`);
+    });
+
+    return tree;
+  }
+}
+
+function installDependencies(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask());
+    context.logger.info(`🔍 Installing packages...`);
+
+    return tree;
+  }
+}
+
+export default function(): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    return chain([
+      addPackageJsonDependencies(),
+      installDependencies()
+    ])(tree, _context);
+  }
+}
