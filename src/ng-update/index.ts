96c826a295fd511af80fdf31c06680ac35ac1227
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-10-12 17:22:02 +0200

    feat(update): prepare schematic

--- src/ng-update/index.ts
+++ src/ng-update/index.ts
@@ -1,9 +1,27 @@
-import { HostTree, Rule } from '@angular-devkit/schematics';
+import {
+  apply,
+  applyTemplates,
+  chain,
+  forEach,
+  HostTree,
+  mergeWith,
+  move,
+  Rule,
+  url
+} from '@angular-devkit/schematics';
 import { getAllComponents } from './lib';
+import { strings } from '@angular-devkit/core';
 
 export function updateToV2(): Rule {
   return (tree: HostTree) => {
-    console.log(getAllComponents(tree));
-    return tree;
-  }
+    const specFileSources = getAllComponents(tree).map(component => {
+      return apply(url('./files'), [
+        applyTemplates({ ...component, ...strings }),
+        move(component.path),
+        forEach(entry => (tree.exists(entry.path) ? null : entry))
+      ]);
+    });
+
+    return chain(specFileSources.map(source => mergeWith(source)));
+  };
 }
--- src/ng-update/lib.ts
+++ src/ng-update/lib.ts
@@ -1,17 +1,27 @@
 import { DirEntry, FileEntry, HostTree } from '@angular-devkit/schematics';
+import { basename, dirname } from 'path';
 
-export function getAllComponents(host:HostTree): FileEntry[] {
-  return getSubFilesRecursively(host).filter(file => file.path.endsWith('component.ts'))
+interface ComponentMeta {
+  name: string;
+  path: string;
+}
+
+export function getAllComponents(host: HostTree): ComponentMeta[] {
+  return getSubFilesRecursively(host)
+    .filter(file => file.path.endsWith('component.ts'))
+    .map(component => ({
+      name: basename(component.path).replace('.component.ts', ''),
+      path: dirname(component.path)
+    }));
 }
 
 export function getSubFilesRecursively(host: HostTree): FileEntry[] {
   function _recurse(entry: DirEntry): FileEntry[] {
-    return entry.subdirs.reduce((files, subDir) => [
-      ...files,
-      ..._recurse(entry.dir(subDir)),
-    ], entry.subfiles.map(subFile => entry.file(subFile) as FileEntry));
+    return entry.subdirs.reduce(
+      (files, subDir) => [...files, ..._recurse(entry.dir(subDir))],
+      entry.subfiles.map(subFile => entry.file(subFile) as FileEntry)
+    );
   }
 
   return _recurse(host.getDir('.'));
 }
-
